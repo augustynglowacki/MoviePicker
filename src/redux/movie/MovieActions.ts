3a292a95f1859df89e68adbb1c2a770065ec2264
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2021-07-19 15:01:17 +0200

    refactored redux action

--- src/redux/movie/MovieActions.ts
+++ src/redux/movie/MovieActions.ts
@@ -1,21 +1,19 @@
 import {API_KEY} from '@env';
 import {createAsyncThunk} from '@reduxjs/toolkit';
 import axiosInstance from '../../helpers/axiosInstance';
+import {convertResponseToMovie} from '../../helpers/convertResponseToMovie';
 import {Movie, MovieAxiosResponse} from '../../models';
 
 //Thunk action
 
-export const getMovies = createAsyncThunk('movie/getMovies', async () => {
-  const res = await axiosInstance.get<MovieAxiosResponse>(
-    `movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
-  );
-  //Mapping our res.data.results, which return full details about movie to a new array which only has the properties that we need.
-  const newresult: Movie[] = res.data.results.map((x: any) => ({
-    id: x.id,
-    title: x.title,
-    vote_average: x.vote_average,
-    poster_path: x.poster_path,
-    overview: x.overview,
-  }));
-  return newresult;
-});
+export const getMovies = createAsyncThunk<Movie[]>(
+  'movie/getMovies',
+  async () => {
+    const res = await axiosInstance.get<MovieAxiosResponse>(
+      `movie/popular?api_key=${API_KEY}&language=en-US&page=1`,
+    );
+
+    const newresult: Movie[] = convertResponseToMovie(res.data.results);
+    return newresult;
+  },
+);
