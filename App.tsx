b5324db091999cccdb40a18a758b1451d7465f23
augustyn1331 <augustyn1331@gmail.com>
2021-07-20 09:35:10 +0200

    black background set globally

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
+import {DefaultTheme, NavigationContainer} from '@react-navigation/native';
 import HomeNavigator from './src/navigation/HomeNavigator';
+import colors from './src/assets/theme/colors';
+
+//app background changed to black
+const MyTheme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: colors.primary,
+    background: colors.black,
+  },
+};
 
 const App = () => {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={MyTheme}>
       <HomeNavigator />
     </NavigationContainer>
   );
