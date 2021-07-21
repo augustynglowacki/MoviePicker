4b8633eddca0fb488a0d02e4a981cdcc8722b5ba
augustyn1331 <augustyn1331@gmail.com>
2021-07-21 12:53:25 +0200

    Login and Register screens logo animation

--- src/components/organisms/Register.tsx
+++ src/components/organisms/Register.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import {Image, StyleSheet, Text, View} from 'react-native';
 import {TextInput} from 'react-native-paper';
+import Animated, {AnimatedLayout, StretchInX} from 'react-native-reanimated';
 import colors from '../../assets/theme/colors';
 import {RegisterForm} from '../../models';
 import Container from '../atoms/Container';
@@ -17,41 +18,46 @@ interface IProps {
 const RegisterComponent = ({onChange, onSubmit, form, errors}: IProps) => {
   return (
     <Container withKeyboard withPadding>
-      <Image
-        source={require('../../assets/images/logo.png')}
-        style={styles.logoImage}
-      />
-      <View>
-        <Text style={styles.title}>Welcome to MoviePicker!</Text>
-        <View style={styles.form}>
-          <Input
-            label="Username"
-            value={form.username}
-            onChangeText={value => onChange({name: 'username', value})}
-            error={errors.username}
+      <AnimatedLayout>
+        <Animated.View entering={StretchInX.springify()}>
+          <Image
+            source={require('../../assets/images/logo.png')}
+            style={styles.logoImage}
           />
-          <Input
-            label="Email"
-            value={form.email}
-            onChangeText={value => onChange({name: 'email', value})}
-            error={errors.email}
-          />
-          <Input
-            label="Password"
-            value={form.password}
-            onChangeText={value => onChange({name: 'password', value})}
-            error={errors.password}
-            secureTextEntry={false}
-            right={<TextInput.Icon name="eye" color={colors.grey} />}
-          />
-          <CustomButton
-            onPress={onSubmit}
-            label="Register"
-            width="small"
-            variant="primary"
-          />
+        </Animated.View>
+
+        <View>
+          <Text style={styles.title}>Welcome to MoviePicker!</Text>
+          <View style={styles.form}>
+            <Input
+              label="Username"
+              value={form.username}
+              onChangeText={value => onChange({name: 'username', value})}
+              error={errors.username}
+            />
+            <Input
+              label="Email"
+              value={form.email}
+              onChangeText={value => onChange({name: 'email', value})}
+              error={errors.email}
+            />
+            <Input
+              label="Password"
+              value={form.password}
+              onChangeText={value => onChange({name: 'password', value})}
+              error={errors.password}
+              secureTextEntry={false}
+              right={<TextInput.Icon name="eye" color={colors.grey} />}
+            />
+            <CustomButton
+              onPress={onSubmit}
+              label="Register"
+              width="small"
+              variant="primary"
+            />
+          </View>
         </View>
-      </View>
+      </AnimatedLayout>
     </Container>
   );
 };
