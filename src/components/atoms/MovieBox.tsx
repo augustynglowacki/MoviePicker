af775f1d8b074ea11132665a56f124360e1c9527
Paweł Sławuta <49125004+Skwlx@users.noreply.github.com>
2021-07-21 12:36:06 +0200

    DiscoveryScreenRedux (#41)
    * Added error handling screen
    
    * Pull from searcherrorbox
    
    * Input component, r-n-paper installed, n-base uninstalled
    
    * New discovery input
    
    * Fixed margins
    
    * clear text on ios feature
    
    * Fixed error in global state. Added some styles change
    
    * autoFocus option
    
    * New CustomButton
    
    * Inputs replaced in Login & Register screens
    
    * Added tap hanlder on movie
    
    * fixed contaier padding
    
    * Added animation
    
    Co-authored-by: augustyn1331 <augustyn1331@gmail.com>

--- src/components/atoms/MovieBox.tsx
+++ src/components/atoms/MovieBox.tsx
@@ -1,22 +1,54 @@
 import {API_IMAGES} from '@env';
-import React from 'react';
-import {View, StyleSheet, ImageBackground} from 'react-native';
+import {useNavigation} from '@react-navigation/native';
+import {DETAILS} from '../../models/constants/routeNames';
+import React, {useEffect, useRef} from 'react';
+import {StyleSheet, ImageBackground, Animated} from 'react-native';
+import {TapGestureHandler} from 'react-native-gesture-handler';
 import colors from '../../assets/theme/colors';
+import {Movie} from '../../models';
+import LinearGradient from 'react-native-linear-gradient';
 
 interface MovieBoxProps {
-  poster_path: string;
+  movie: Movie;
 }
 
-const MovieBox = ({poster_path}: MovieBoxProps) => {
+const MovieBox = ({movie}: MovieBoxProps) => {
+  const fadeAnim = useRef(new Animated.Value(0)).current;
+  const {navigate} = useNavigation();
+
+  useEffect(() => {
+    Animated.timing(fadeAnim, {
+      toValue: 1,
+      duration: 900,
+      useNativeDriver: true,
+    }).start();
+  }, [fadeAnim]);
+
   return (
     <>
-      {!!poster_path && (
-        <View style={styles.movieBox}>
-          <ImageBackground
-            source={{uri: `${API_IMAGES}${poster_path}`}}
-            style={styles.movieImage}
-          />
-        </View>
+      {!!movie.poster_path && (
+        <TapGestureHandler
+          onActivated={() => {
+            navigate(DETAILS, {
+              poster_path: movie.poster_path,
+              overview: movie.overview,
+              title: movie.title,
+              id: movie.id,
+            });
+          }}>
+          <Animated.View style={{...styles.movieBox, opacity: fadeAnim}}>
+            <ImageBackground
+              source={{uri: `${API_IMAGES}${movie.poster_path}`}}
+              style={styles.movieImage}
+            />
+            <LinearGradient
+              colors={['rgba(0,0,0,0.4)', 'rgba(0,0,0,0.1)', 'rgba(0,0,0,0.1)']}
+              start={{x: 0, y: 1}}
+              end={{x: 0, y: 0}}
+              style={styles.linearGradient}
+            />
+          </Animated.View>
+        </TapGestureHandler>
       )}
     </>
   );
@@ -28,13 +60,19 @@ const styles = StyleSheet.create({
     height: 250,
     flexBasis: '45%',
     margin: 3,
-    backgroundColor: colors.primary,
+    backgroundColor: colors.black,
   },
   movieImage: {
     width: '100%',
     height: '100%',
     resizeMode: 'cover',
     position: 'absolute',
+  },
+  linearGradient: {
+    height: '100%',
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
   },
 });
 
--- src/components/molecules/DiscoveryContentBox.tsx
+++ src/components/molecules/DiscoveryContentBox.tsx
@@ -4,27 +4,38 @@ import {FlatList} from 'react-native-gesture-handler';
 import colors from '../../assets/theme/colors';
 import {Movie} from '../../models';
 import MovieBox from '../atoms/MovieBox';
+import SearchErrorBox from '../atoms/SearchErrorBox';
 import SectionHeader from '../atoms/SectionHeader';
 
 interface DiscoveryContentBoxProps {
   title: string;
   data: Movie[];
+  error: string;
+  loading: boolean;
 }
 
-const renderItem: ListRenderItem<Movie> = ({item}) => (
-  <MovieBox poster_path={item.poster_path} />
-);
+const renderItem: ListRenderItem<Movie> = ({item}) => <MovieBox movie={item} />;
 
-const DiscoveryContentBox = ({title, data}: DiscoveryContentBoxProps) => {
+const DiscoveryContentBox = ({
+  title,
+  data,
+  error,
+}: DiscoveryContentBoxProps) => {
   return (
     <View style={styles.discoveryContentBox}>
-      <SectionHeader text={title} size={20} color={colors.white} />
-      <FlatList
-        data={data}
-        renderItem={renderItem}
-        horizontal={true}
-        showsHorizontalScrollIndicator={false}
-      />
+      {data.length === 0 ? null : (
+        <SectionHeader text={title} size={20} color={colors.white} />
+      )}
+      <SearchErrorBox error={error} loading={false}>
+        <FlatList
+          data={data}
+          renderItem={renderItem}
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          maxToRenderPerBatch={5}
+          scrollsToTop={true}
+        />
+      </SearchErrorBox>
     </View>
   );
 };
