354d38803c0a09358be5ba67600aaba5b9befb05
augustyn1331 <augustyn1331@gmail.com>
2021-07-22 11:57:55 +0200

    Merge branch 'dev'

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,7 +4,6 @@
 
 import 'react-native';
 import React from 'react';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 import SearchErrorBox from '../src/components/atoms/SearchErrorBox';
