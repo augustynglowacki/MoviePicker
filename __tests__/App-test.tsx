d08b0a2e5fc0366e0af6bc75f6f26ec71b1b3371
Augustyn Głowacki <71926817+augustyn1331@users.noreply.github.com>
2021-07-22 12:01:18 +0200

    Unit tests initial config (#52)
    * test without imports working
    
    * unit testing config
    
    * Merge branch 'dev'

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,11 +4,13 @@
 
 import 'react-native';
 import React from 'react';
-import App from '../App';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import SearchErrorBox from '../src/components/atoms/SearchErrorBox';
+import {Text} from 'react-native';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderer.create(
+    <SearchErrorBox loading error="" children={<Text>dds</Text>} />,
+  );
 });
