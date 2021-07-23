a2b9a759605af8caf831094ff8cb1462ea6c68d7
plotrekhq <plotrekhqpl@gmail.com>
2021-07-23 23:54:20 +0200

    firestore config

--- src/screens/Liked.tsx
+++ src/screens/Liked.tsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {View, StyleSheet} from 'react-native';
-
-import LikedContentBox from '../components/molecules/LikedContentBox';
+import firestore from '@react-native-firebase/firestore';
+import {Text} from 'react-native';
 
 const Liked = () => {
+  const [state, setState] = useState([]);
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const db = firestore();
+    const data = await db.collection('liked').get();
+    setState(data.docs.map(doc => doc.data()));
+  };
+
   return (
     <View style={styles.wrapper}>
-      <LikedContentBox />
+      {state.map(stat => (
+        <Text>{stat.id}</Text>
+      ))}
     </View>
   );
 };
@@ -15,7 +28,7 @@ export default Liked;
 
 const styles = StyleSheet.create({
   wrapper: {
-    backgroundColor: 'black',
+    backgroundColor: 'white',
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
