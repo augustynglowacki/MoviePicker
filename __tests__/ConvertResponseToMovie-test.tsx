59ac9987f9da191d0344d896698fd7a040d2a558
Paweł Sławuta <49125004+Skwlx@users.noreply.github.com>
2021-07-23 13:26:57 +0200

    Tests and ios config (#57)
    * Added 3 tests
    
    * Working google sign in button

---
+++ __tests__/ConvertResponseToMovie-test.tsx
@@ -0,0 +1,28 @@
+import {convertResponseToMovie} from '../src/helpers/convertResponseToMovie';
+import {Movie} from '../src/models';
+
+test('Checks if Conversion to array of movies is succeeded', () => {
+  const response = [
+    {
+      title: '',
+      poster_path: '',
+      id: 0,
+      overview: '',
+      genre_ids: [],
+      actors: '',
+      vote_average: 0,
+      poster: '',
+    },
+  ];
+  const convertedResponse: Movie[] = [
+    {
+      title: '',
+      poster_path: '',
+      id: 0,
+      overview: '',
+      genre_ids: [],
+      vote_average: 0,
+    },
+  ];
+  expect(convertResponseToMovie(response)).toStrictEqual(convertedResponse);
+});
