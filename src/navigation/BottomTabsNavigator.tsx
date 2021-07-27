ecc28e7b445fbd7f2bfae8ea0154ca0c76242018
plotrekhq <plotrekhqpl@gmail.com>
2021-07-27 13:54:43 +0200

    Not logged in navigation

--- src/navigation/BottomTabsNavigator.tsx
+++ src/navigation/BottomTabsNavigator.tsx
@@ -7,10 +7,17 @@ import TabIcon from '../components/atoms/TabIcon';
 import {DISCOVER, HOME, LIKED, PROFILE} from '../models/constants/routeNames';
 import {StyleSheet} from 'react-native';
 import Profile from '../screens/Profile';
+import {useSelector} from 'react-redux';
+import {userThunkSelector} from '../redux/user/UserSlice';
+
+import NotLoggedIn from '../screens/NotLoggedIn';
 
 const Tab = createBottomTabNavigator();
 
 const BottomTabsNavigator = () => {
+  const {
+    user: {email},
+  } = useSelector(userThunkSelector);
   return (
     <Tab.Navigator
       tabBarOptions={{
@@ -31,20 +38,42 @@ const BottomTabsNavigator = () => {
           tabBarIcon: ({focused}) => TabIcon(focused, 'search'),
         }}
       />
-      <Tab.Screen
-        name={LIKED}
-        component={Liked}
-        options={{
-          tabBarIcon: ({focused}) => TabIcon(focused, 'favorite-outline'),
-        }}
-      />
-      <Tab.Screen
-        name={PROFILE}
-        component={Profile}
-        options={{
-          tabBarIcon: ({focused}) => TabIcon(focused, 'person-outline'),
-        }}
-      />
+
+      {email !== '' ? (
+        <>
+          <Tab.Screen
+            name={LIKED}
+            component={Liked}
+            options={{
+              tabBarIcon: ({focused}) => TabIcon(focused, 'favorite-outline'),
+            }}
+          />
+          <Tab.Screen
+            name={PROFILE}
+            component={Profile}
+            options={{
+              tabBarIcon: ({focused}) => TabIcon(focused, 'person-outline'),
+            }}
+          />
+        </>
+      ) : (
+        <>
+          <Tab.Screen
+            name={LIKED}
+            children={() => <NotLoggedIn isLiked={true} />}
+            options={{
+              tabBarIcon: ({focused}) => TabIcon(focused, 'favorite-outline'),
+            }}
+          />
+          <Tab.Screen
+            name={PROFILE}
+            component={NotLoggedIn}
+            options={{
+              tabBarIcon: ({focused}) => TabIcon(focused, 'person-outline'),
+            }}
+          />
+        </>
+      )}
     </Tab.Navigator>
   );
 };
