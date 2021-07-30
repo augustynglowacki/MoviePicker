94123436cea0909fcc4ca8953da782d0e3c71ce5
augustyn1331 <augustyn1331@gmail.com>
2021-07-30 22:16:40 +0200

    Minor config changes

---
+++ src/styles/palette.ts
@@ -0,0 +1,10 @@
+export default {
+  primary: '#FFA31A',
+  grey: '#808080',
+  darkGrey: '#292929',
+  black: '#1b1b1b',
+  white: '#ffffff',
+  danger: '#A40001', //error color
+  strongBlack: '#000000',
+  lightGrey: '#999999',
+};
