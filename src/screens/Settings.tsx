16a662ab40a7937a6a0cf30c76f5a4fd7029999e
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2021-08-05 13:51:17 +0200

    Removed: Settingsbox Migrate: Settingbox to settings screen

--- src/screens/Settings.tsx
+++ src/screens/Settings.tsx
@@ -1,122 +1,104 @@
-import React from 'react';
-import * as Yup from 'yup';
-import {useTranslation} from 'react-i18next';
-import {useDispatch, useSelector} from 'react-redux';
-import {setActiveUser, userThunkSelector} from 'src/redux/user/UserSlice';
-import {useFormik} from 'formik';
-import auth from '@react-native-firebase/auth';
+import React, {useEffect, useState} from 'react';
 import {useNavigation} from '@react-navigation/native';
-import SettingsBox from 'src/components/settings/SettingsBox';
-import {updateUserEmail, updateUserPassword} from 'src/redux/user/UserAction';
-import {MIN_PASSWORD_LENGTH} from './Register';
-import {Route} from 'src/constants';
+import storage from '@react-native-firebase/storage';
+import auth from '@react-native-firebase/auth';
+import {Container, SectionHeader} from 'src/components/common';
 
-export interface UpdateUserFormValues {
-  displayEmail: boolean;
-  email: string;
-  newEmail: string;
-  password: string;
-  newPassword: string;
-  displayName: string;
-}
+import {IconTypes, Route} from 'src/constants';
+
+import {userThunkSelector} from 'src/redux/user/UserSlice';
+import {useSelector} from 'react-redux';
+
+import palette from 'src/styles/palette';
+import UserNameForm from 'src/components/settings/UserNameForm';
+import UserEmailForm from 'src/components/settings/UserEmailForm';
+import UserPasswordForm from 'src/components/settings/UserPasswordForm';
+import HeaderBar from 'src/components/common/HeaderBar';
+import Avatar from 'src/components/settings/Avatar';
+import SettingOptionBox from 'src/components/settings/SettingOptionBox';
+import ChangeBackground from 'src/components/settings/ChangeBackground';
 
 const Settings: React.FC = () => {
+  const [profileURI, setProfileURI] = useState<string>(
+    'https://firebasestorage.googleapis.com/v0/b/moviepicker-2405b.appspot.com/o/users%2Fdefault%2FdefaultProfile.jpeg?alt=media&token=bc972054-6f70-4339-a72d-4a6c89be93a2',
+  );
   const {user, error, loading} = useSelector(userThunkSelector);
-  const dispatch = useDispatch();
-  const {t} = useTranslation('form');
   const {navigate} = useNavigation();
-  const yupEmail = Yup.string().email(t('email'));
-  const yupPassword = Yup.string().min(
-    MIN_PASSWORD_LENGTH,
-    t('short', {MIN_PASSWORD_LENGTH}),
-  );
+  const [displayUserName, setDisplayUserName] = useState<boolean>(false);
+  const [displayUserEmail, setDisplayUserEmail] = useState<boolean>(false);
+  const [displayUserPassword, setDisplayUserPassword] =
+    useState<boolean>(false);
   const redirectToProfile = () => navigate(Route.PROFILE);
+  const leftIcon = {
+    name: 'arrow-back-ios',
+    type: IconTypes.MATERIAL,
+    onPressFunction: () => redirectToProfile(),
+  };
 
-  const onSubmit = () => {
-    const displayNameFirebase = auth().currentUser?.displayName;
-    if (values.displayName !== displayNameFirebase) {
-      handleUserNameUpdate(values);
+  const fetchAvatar = async () => {
+    try {
+      const userId = auth().currentUser?.uid;
+      const results = await storage()
+        .ref(`/users/${userId}/profile.jpg`)
+        .getDownloadURL();
+      setProfileURI(results);
+    } catch (err) {
+      console.log(err);
     }
-    if (values.newEmail) {
-      handleUserEmailUpdate(values);
-    }
-    if (values.newPassword) {
-      handleUserPasswordUpdate(values);
-    }
   };
+  useEffect(() => {
+    fetchAvatar();
+  }, []);
 
-  const handleUserEmailUpdate = (values: UpdateUserFormValues) => {
-    dispatch(
-      updateUserEmail({
-        ...values,
-        callback: redirectToProfile,
-      }),
+  if (displayUserName) {
+    return (
+      <UserNameForm
+        userName={user.userName}
+        goBackFunction={() => setDisplayUserName(false)}
+        error={error}
+        loading={loading}
+      />
     );
-  };
+  }
 
-  const handleUserPasswordUpdate = (values: UpdateUserFormValues) => {
-    dispatch(
-      updateUserPassword({
-        ...values,
-        callback: redirectToProfile,
-      }),
+  if (displayUserEmail) {
+    return (
+      <UserEmailForm
+        goBackFunction={() => setDisplayUserEmail(false)}
+        userEmail={user.email}
+        error={error}
+        loading={loading}
+      />
     );
-  };
-
-  const handleUserNameUpdate = async ({displayName}: UpdateUserFormValues) => {
-    await auth().currentUser?.updateProfile({
-      displayName,
-    });
-    dispatch(
-      setActiveUser({
-        userName: displayName,
-      }),
+  }
+  if (displayUserPassword) {
+    return (
+      <UserPasswordForm
+        goBackFunction={() => setDisplayUserPassword(false)}
+        error={error}
+        loading={loading}
+      />
     );
-    navigate(Route.PROFILE);
-  };
-
-  const validationSchema = Yup.object({
-    displayEmail: Yup.boolean(),
-    displayName: Yup.string().min(3), // add const for this min values
-    email: yupEmail,
-    newEmail: yupEmail.nope([user.email]).when('displayEmail', {
-      is: (val: boolean) => val,
-      then: yupEmail.required('Insert new email'),
-    }),
-    password: yupPassword.required(t('required')),
-    newPassword: yupPassword.when('displayEmail', {
-      is: (val: boolean) => !val,
-      then: yupPassword.required('Insert new password'),
-    }),
-  });
-
-  const {setFieldValue, handleChange, handleSubmit, errors, values} = useFormik(
-    {
-      initialValues: {
-        displayEmail: true,
-        email: user.email,
-        newEmail: '',
-        password: '',
-        newPassword: '',
-        displayName: user.userName,
-      }, // create initial const
-      validationSchema,
-      validateOnChange: false,
-      validateOnBlur: false,
-      onSubmit,
-    },
-  );
-
+  }
   return (
-    <SettingsBox
-      values={values}
-      onSubmit={handleSubmit}
-      onChange={handleChange}
-      fieldValue={setFieldValue}
-      serverError={error}
-      errors={errors}
-      loading={loading}
-    />
+    <Container flexStart>
+      <HeaderBar leftIcon={leftIcon} />
+      <SectionHeader text="Update your profile" color={palette.white} center />
+      <Avatar uri={profileURI} editable />
+      <ChangeBackground />
+      <SettingOptionBox
+        text="Change Username"
+        navigateTo={() => setDisplayUserName(true)}
+      />
+      <SettingOptionBox
+        text="Change Email"
+        navigateTo={() => setDisplayUserEmail(true)}
+      />
+      <SettingOptionBox
+        text="Change Password"
+        navigateTo={() => setDisplayUserPassword(true)}
+      />
+    </Container>
   );
 };
 
