16a6dbe531c24dcbd722d8800f97f48918c46e9a
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2021-08-05 13:48:35 +0200

    add: Header bar

---
+++ src/components/common/HeaderBar.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import {StyleSheet, TouchableOpacity, View} from 'react-native';
+import palette from 'src/styles/palette';
+import {IconTypes} from 'src/constants';
+import {Icon} from '../common';
+
+interface Props {
+  leftIcon: {
+    type: IconTypes;
+    name: string;
+    onPressFunction: () => void;
+  };
+  rightIcon?: {
+    type: IconTypes;
+    name: string;
+    onPressFunction: () => void;
+  };
+}
+
+const HeaderBar: React.FC<Props> = ({leftIcon, rightIcon}) => {
+  return (
+    <View style={styles.titleBar}>
+      <TouchableOpacity onPress={leftIcon.onPressFunction}>
+        <Icon
+          type={leftIcon.type}
+          color={palette.white}
+          name={leftIcon.name}
+          size={26}
+        />
+      </TouchableOpacity>
+      {rightIcon && (
+        <TouchableOpacity onPress={rightIcon.onPressFunction}>
+          <Icon
+            type={rightIcon.type}
+            color={palette.white}
+            name={rightIcon.name}
+            size={26}
+          />
+        </TouchableOpacity>
+      )}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  titleBar: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 15,
+    marginHorizontal: 16,
+    color: 'white',
+  },
+});
+export default HeaderBar;
