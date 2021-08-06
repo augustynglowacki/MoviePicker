e16d1562e7db2f8534ff07a4e14e3e06b3d56727
skwlx.xx@o2.pl <skwlx.xx@o2.pl>
2021-08-06 11:41:28 +0200

    Add: userformtemplate

---
+++ src/components/settings/UserFormTemplate.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import {IconTypes} from 'src/constants';
+import {UserFormDataTemplate} from 'src/models';
+import palette from 'src/styles/palette';
+import {
+  Container,
+  CustomButton,
+  HeaderBar,
+  Message,
+  SectionHeader,
+} from '../common';
+import SettingInput from './SettingInput';
+
+interface Props {
+  goBack: () => void;
+  onSubmit: () => void;
+  headerText: string;
+  formData: UserFormDataTemplate[];
+  serverError: string;
+  loading: boolean;
+}
+
+const UserFormTemplate: React.FC<Props> = ({
+  goBack,
+  headerText,
+  formData,
+  onSubmit,
+  loading,
+  serverError,
+}) => {
+  const leftIcon = {
+    name: 'arrow-back-ios',
+    type: IconTypes.MATERIAL,
+    onPressFunction: goBack,
+  };
+  return (
+    <Container flexStart>
+      <HeaderBar leftIcon={leftIcon} />
+      <SectionHeader text={headerText} color={palette.white} center />
+      {formData.map(formElement => (
+        <SettingInput
+          label={formElement.label}
+          initialValue={formElement.initialValue}
+          onChange={formElement.onChange}
+          error={formElement.error}
+          secureTextEntry={formElement.secure}
+        />
+      ))}
+      <CustomButton
+        variant="primary"
+        label="Save"
+        onPress={onSubmit}
+        width="medium"
+        loading={loading}
+      />
+      {!!serverError && <Message label={serverError} />}
+    </Container>
+  );
+};
+
+export default UserFormTemplate;
