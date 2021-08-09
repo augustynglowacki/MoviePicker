3f518daa63640901500b4af45d879d7780518c42
Piotr Krysiak <plotrekhqpl@gmail.com>
2021-08-09 15:03:38 +0200

    MP-047 Go back gesture enabled (#103)

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -3,3 +3,8 @@ import {Dimensions} from 'react-native';
 export const BOTTOM_TABS_HEIGHT = 60;
 export const WINDOW_HEIGHT = Dimensions.get('window').height;
 export const HEADER_HEIGHT = 300;
+export const SCREEN_OPTIONS = {
+  headerShown: false,
+  gestureEnabled: true,
+  gestureResponseDistance: {horizontal: 200},
+};
--- src/navigation/HomeNavigator.tsx
+++ src/navigation/HomeNavigator.tsx
@@ -1,36 +1,23 @@
 import {createStackNavigator} from '@react-navigation/stack';
 import React from 'react';
-import {Route} from 'src/constants';
+import {Route, SCREEN_OPTIONS} from 'src/constants';
 import DetailsScreen from 'src/screens/DetailsScreen';
 import SettingsScreen from 'src/screens/SettingsScreen';
-import BottomTabsNavigator from 'src/navigation/BottomTabs/BottomTabsNavigator';
 import AuthNavigator from './AuthNavigator';
+import BottomTabsNavigator from './BottomTabs/BottomTabsNavigator';
 
 const Stack = createStackNavigator();
 
 const HomeNavigator = () => {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={SCREEN_OPTIONS}>
       <Stack.Screen
-        options={{headerShown: false}}
         name={Route.HOME_NAVIGATOR}
         component={BottomTabsNavigator}
       />
-      <Stack.Screen
-        options={{headerShown: false}}
-        name={Route.AUTH}
-        component={AuthNavigator}
-      />
-      <Stack.Screen
-        name={Route.DETAILS}
-        component={DetailsScreen}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
-        name={Route.SETTINGS}
-        component={SettingsScreen}
-        options={{headerShown: false}}
-      />
+      <Stack.Screen name={Route.AUTH} component={AuthNavigator} />
+      <Stack.Screen name={Route.DETAILS} component={DetailsScreen} />
+      <Stack.Screen name={Route.SETTINGS} component={SettingsScreen} />
     </Stack.Navigator>
   );
 };
