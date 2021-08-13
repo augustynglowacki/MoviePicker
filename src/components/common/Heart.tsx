3a74426009ef01b64ef6ae638f301ac9a6868d58
plotrekhq <plotrekhqpl@gmail.com>
2021-08-13 13:38:10 +0200

    Heart lottie

--- src/components/common/Heart.tsx
+++ src/components/common/Heart.tsx
@@ -1,48 +1,13 @@
-import React, {useEffect, useRef} from 'react';
-import {StyleSheet, Animated, Image} from 'react-native';
-import palette from 'src/styles/palette';
+import React from 'react';
+import {StyleSheet, Animated} from 'react-native';
+import AnimatedLottieView from 'lottie-react-native';
 
 const Heart: React.FC = () => {
-  const maxValue = 1.4;
-  const minValue = 1;
-  const duration = 200;
-  const beatAnim = useRef(new Animated.Value(1)).current;
-  const fadeAnim = useRef(new Animated.Value(0)).current;
-
-  useEffect(() => {
-    Animated.sequence([
-      Animated.timing(fadeAnim, {
-        toValue: 1,
-        duration,
-        useNativeDriver: true,
-      }),
-      Animated.timing(beatAnim, {
-        toValue: maxValue,
-        duration,
-        useNativeDriver: true,
-      }),
-      Animated.timing(beatAnim, {
-        toValue: minValue,
-        duration,
-        useNativeDriver: true,
-      }),
-      Animated.timing(fadeAnim, {
-        toValue: 0,
-        duration,
-        useNativeDriver: true,
-      }),
-    ]).start();
-  }, [fadeAnim, beatAnim]);
-
   return (
-    <Animated.View
-      style={[
-        styles.heartIconBox,
-        {opacity: fadeAnim, transform: [{scale: beatAnim}]},
-      ]}>
-      <Image
-        source={require('src/assets/images/logo.png')}
-        style={styles.logoImage}
+    <Animated.View style={styles.heartIconBox}>
+      <AnimatedLottieView
+        source={require('../../assets/lottie/heart5.json')}
+        autoPlay
       />
     </Animated.View>
   );
@@ -51,21 +16,9 @@ const Heart: React.FC = () => {
 const styles = StyleSheet.create({
   heartIconBox: {
     position: 'absolute',
-    alignSelf: 'center',
-    shadowOpacity: 1,
-    shadowRadius: 50,
-    shadowColor: palette.strongBlack,
-    shadowOffset: {
-      width: 0,
-      height: 0,
-    },
-    elevation: 50,
-  },
-  logoImage: {
-    height: 100,
-    width: 100,
-    alignSelf: 'center',
-    opacity: 0.95,
+    width: 300,
+    height: 300,
+    margin: 20,
   },
 });
 export default Heart;
