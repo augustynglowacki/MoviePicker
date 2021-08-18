e28e79fbf751b2dd8940390f165c9f49adc67c0b
augustyn1331 <augustyn1331@gmail.com>
2021-08-18 08:50:30 +0200

    Navigation problem resolved

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,20 +1,26 @@
+/* eslint-disable no-undef */
 import Enzyme from 'enzyme';
 import EnzymeAdapter from '@wojtekmaj/enzyme-adapter-react-17';
+import 'react-native-gesture-handler/jestSetup';
 
+// Native event firebase mock
 jest.mock('react-native/Libraries/EventEmitter/NativeEventEmitter');
-// jest.mock('@react-navigation/native');
-// jest.mock('react-native-reanimated', () => {
-//   // eslint-disable-next-line @typescript-eslint/no-var-requires
-//   const Reanimated = require('react-native-reanimated/mock');
 
-//   // The mock for call immediately calls the callback which is incorrect
-//   // So we override it with a no-op
-//   // eslint-disable-next-line @typescript-eslint/no-empty-function
-//   Reanimated.default.call = () => {};
-//   return Reanimated;
-// });
+//Reanimated mock, required for navigation according to navigation docs
+jest.mock('react-native-reanimated', () => {
+  const Reanimated = require('react-native-reanimated/mock');
 
+  // The mock for call immediately calls the callback which is incorrect
+  // So we override it with a no-op
+  Reanimated.default.call = () => {};
+  return Reanimated;
+});
 // Silence the warning: Animated: useNativeDriver is not supported because the native animated module is missing
-// jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
+// Navigation mock
+const mockedNavigate = jest.fn();
+jest.mock('@react-navigation/native', () => ({
+  useNavigation: () => ({navigate: mockedNavigate}),
+}));
 
 Enzyme.configure({adapter: new EnzymeAdapter()});
